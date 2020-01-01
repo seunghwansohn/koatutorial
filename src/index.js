2db5e20de04c6ca1ab336a90966a6abd6ad0097f
Seunghwan <seunghwansohn@gmail.com>
2020-01-01 13:46:32 +0700

    3. Koa에 async/await 적용

--- src/index.js
+++ src/index.js
@@ -2,17 +2,15 @@ const Koa = require('koa')
 
 const app = new Koa()
 
-app.use((ctx, next) => {
+app.use(async(ctx, next) => { //async적용
     console.log(ctx.url);
     console.log(1);
     if (ctx.query.authorized !== '1') { ///?authorized=1로 접근할 떄만 hello world출력
         ctx.status = 401; //Koa 에서는 401은 페이지에 Unauthorized를 출력함
         return;
     }
-    next().then(() => { //?authorized=1로 접근 성공 이후 콘솔로그에 END출력.
-                        //next함수가 promise함수라는 것을 보여줌
-        console.log('END')
-    });
+    await next() //await 적용 ?authorized=1로 접근 성공 이후 콘솔로그에 END출력.
+    console.log('END')
 })
 
 app.use((ctx, next) => {
