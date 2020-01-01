5482c9503a5ed5de7a14985ec293907098dedd14
Seunghwan <seunghwansohn@gmail.com>
2020-01-01 17:49:09 +0700

    11. 몽고디비 접속 완료

--- src/index.js
+++ src/index.js
@@ -1,8 +1,29 @@
+require('dotenv').config();
 const Koa = require('koa')
 const Router = require('koa-router')
 const bodyParser = require('koa-bodyparser')
+const mongoose = require('mongoose')
+
+
+const {PORT, MONGO_URI} = process.env;
 
 const api = require('./api');
+
+
+mongoose.
+createConnection(MONGO_URI, 
+    {
+        useCreateIndex: true,
+        useNewUrlParser: true, 
+        useUnifiedTopology: true,
+        useFindAndModify : false
+    })
+    .then(() => {
+        console.log('Connected to MongoDB')
+    })
+    .catch(e => {
+        console.error(e);
+    })
 
 const app = new Koa()
 const router = new Router();
@@ -16,8 +37,9 @@ app.use(bodyParser())
 //app 인스턴스에 라우터 적용
 app.use(router.routes()).use(router.allowedMethods())
 
+const port = PORT||4000;
 app.listen(4000,() => {
-    console.log('Listening to port 4000')
+    console.log('Listening to %d', port)
 })
 
 //http://192.168.100.4:4000/api/posts
