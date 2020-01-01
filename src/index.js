90819e17f2f90231817e224331c20debf248c1b8
Seunghwan <seunghwansohn@gmail.com>
2020-01-01 14:50:32 +0700

    5. koa-router 적용

--- src/index.js
+++ src/index.js
@@ -1,26 +1,18 @@
 const Koa = require('koa')
+const Router = require('koa-router')
 
 const app = new Koa()
+const router = new Router();
 
-app.use(async(ctx, next) => { //async적용
-    console.log(ctx.url);
-    console.log(1);
-    if (ctx.query.authorized !== '1') { ///?authorized=1로 접근할 떄만 hello world출력
-        ctx.status = 401; //Koa 에서는 401은 페이지에 Unauthorized를 출력함
-        return;
-    }
-    await next() //await 적용 ?authorized=1로 접근 성공 이후 콘솔로그에 END출력.
-    console.log('END')
+//라우터 설정
+router.get('/', ctx => {
+    ctx.body = '홈';
 })
-
-app.use((ctx, next) => {
-    console.log(2);
-    next();
-});
-
-app.use(ctx => {
-    ctx.body = 'hello world';
+router.get('/about', ctx => {
+    ctx.body = '소개'
 })
+//app 인스턴스에 라우터 적용
+app.use(router.routes()).use(router.allowedMethods())
 
 app.listen(4000,() => {
     console.log('Listening to port 4000')
