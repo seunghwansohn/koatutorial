5aba028bd4f1919b91e8f8832437f47f1ffe9a6d
Seunghwan <seunghwansohn@gmail.com>
2020-01-04 09:52:34 +0700

    ObjectId를 통해 id 검증 도입

--- src/api/posts/index.js
+++ src/api/posts/index.js
@@ -5,8 +5,10 @@ const posts = new Router();
 
 posts.get('/', postsCtrl.list);
 posts.post('/', postsCtrl.write);
-posts.get('/:id', postsCtrl.read);
-posts.delete('/:id', postsCtrl.remove);
-posts.patch('/:id', postsCtrl.update);
+posts.get('/:id', postsCtrl.checkObjectId, postsCtrl.read);
+posts.delete('/:id', postsCtrl.checkObjectId, postsCtrl.remove);
+posts.patch('/:id', postsCtrl.checkObjectId, postsCtrl.update);
+//checkObjectId 메소드의 도입으로 500에러(서버내부오류에러)
+//가 404에러 (Bad Request)에러로 나타나게 됨.
 
 export default posts;
