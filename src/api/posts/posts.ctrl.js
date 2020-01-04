696032bac85de912cb6295443d2f8f4efb6ed616
Seunghwan <seunghwansohn@gmail.com>
2020-01-04 10:15:39 +0700

    업데이트에도 검증 도입

--- src/api/posts/posts.ctrl.js
+++ src/api/posts/posts.ctrl.js
@@ -83,6 +83,19 @@ export const remove = async ctx => {
 
 export const update = async ctx => {
   const { id } = ctx.params;
+  const schema = Joi.object().keys({
+    title: Joi.string(),
+    body: Joi.string(),
+    tags: Joi.array().items(Joi.string()),
+  });
+
+  // 검증 후, 검증 실패시 에러처리
+  const result = Joi.validate(ctx.request.body, schema);
+  if (result.error) {
+    ctx.status = 400; // Bad Request
+    ctx.body = result.error;
+    return;
+  }
   try {
     const post = await Post.findByIdAndUpdate(id, ctx.request.body, {
       new: true, // 이 값을 설정하면 업데이트된 데이터를 반환합니다.
