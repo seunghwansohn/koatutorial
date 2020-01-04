7c0b9d8d585d0f9ffb07fdeabf2117e4361a784e
Seunghwan <seunghwansohn@gmail.com>
2020-01-04 09:39:49 +0700

    검증 기능 도입 전. 모든 CRUD 정상작동

--- src/api/posts/posts.ctrl.js
+++ src/api/posts/posts.ctrl.js
@@ -9,6 +9,7 @@ export const write = async ctx => {
   });
   try {
     await post.save();
+    //save는 mongoose의 메소드로서 데이터베이스에 내용을 저장하는 메소드
     ctx.body = post;
   } catch (e) {
     ctx.throw(500, e)
@@ -17,17 +18,55 @@ export const write = async ctx => {
 
 export const list = async ctx => {
   try {
-    const posts = await Post.find().exec();
+    const posts = await Post.find().exec(); 
+    //find는 mongoose의 메소드로서 특별히 실행을 위해서는 exec()도 붙여야됨
     ctx.body = posts;
   } catch (e) {
     ctx.throw(500, e)
   }
 }
 
-export const read = ctx => {}
+export const read = async ctx => {
+  const { id } = ctx.params;
+  try {
+    const post = await Post.findById(id).exec();
+    //주소에서 인자로 전달받은 id에 해당되는 값만 찾는 mongoose의 메소드
+    //주소에서 전달하는 인자는 1,2 식이 아니라 몽고디비에 저장된 아이디
+    if (!post) {
+      ctx.status = 404; // Not Found
+      return;
+    }
+    ctx.body = post;
+  } catch (e) {
+    ctx.throw(500, e);
+  }
+};
 
-export const remove = ctx => {}
+export const remove = async ctx => {
+  const { id } = ctx.params;
+  try {
+    await Post.findByIdAndRemove(id).exec();
+    ctx.status = 204; // No Content (성공은 했지만 응답할 데이터는 없음)
+  } catch (e) {
+    ctx.throw(500, e);
+  }
+}
 
-export const update = ctx => {}
+export const update = async ctx => {
+  const { id } = ctx.params;
+  try {
+    const post = await Post.findByIdAndUpdate(id, ctx.request.body, {
+      new: true, // 이 값을 설정하면 업데이트된 데이터를 반환합니다.
+      // false 일 때에는 업데이트 되기 전의 데이터를 반환합니다.
+    }).exec();
+    if (!post) {
+      ctx.status = 404;
+      return;
+    }
+    ctx.body = post;
+  } catch (e) {
+    ctx.throw(500, e);
+  }
+}
 
 
